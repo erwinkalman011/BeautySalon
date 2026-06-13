d628bc23fca38ba55944d4ff0778acc22ad8183b
Elena Singeorzan <singeorzanelena50@gmail.com>
2026-06-13 15:39:48 +0300

    Elena: Am curatat baza de date si codul

--- BeautySalon.Api/Program.cs
+++ BeautySalon.Api/Program.cs
@@ -11,16 +11,13 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Baza de date
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=BeautySalon.db"));
 
-// 2. Identity
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 
-// 2.5 Configurare JWT (Paznicul)
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -30,8 +27,7 @@ builder.Services.AddAuthentication(options =>
 .AddJwtBearer(options =>
 {
     var jwtSettings = builder.Configuration.GetSection("Jwt");
-    
-    // 1. Parametrii de validare
+
     options.TokenValidationParameters = new TokenValidationParameters
     {
         ValidateIssuer = true,
@@ -43,26 +39,21 @@ builder.Services.AddAuthentication(options =>
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]!))
     };
 
-    // 2. Senzorii de Debug
     options.Events = new JwtBearerEvents
     {
         OnMessageReceived = context =>
         {
-            //Console.WriteLine($"\n 📩 [DEBUG] AM PRIMIT TOKEN-UL: {context.Token}");
             return Task.CompletedTask;
         },
         OnAuthenticationFailed = context =>
         {
-            //Console.WriteLine($"\n ❌ [DEBUG] EROARE VALIDARE TOKEN: {context.Exception.Message}");
             return Task.CompletedTask;
         }
     };
 });
 
-// 3. Serviciile tale
 builder.Services.AddScoped<IAuthService, AuthService>();
 
-// 4. GraphQL
 builder.Services
     .AddGraphQLServer()
     .AddAuthorization()
@@ -79,11 +70,9 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-// Ordinea aici este vitală
 app.UseAuthentication();
 app.UseAuthorization();
 
-// 5. Mapare GraphQL
 app.MapGraphQL();
 
 app.Run();
